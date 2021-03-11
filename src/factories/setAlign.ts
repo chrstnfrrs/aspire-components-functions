c943294b4faf483feea427b73f683b4c6b5d564d
Christian Farris <christianfarriscs@gmail.com>
2021-03-10 21:27:14 -0600

    create setAlign factory

---
+++ src/factories/setAlign.ts
@@ -0,0 +1,11 @@
+import { IFactoryFunction, FactoryMap } from '../types';
+
+const map: FactoryMap = {
+  baseline: 'align-baseline',
+  center: 'align-center',
+  end: 'align-end',
+  start: 'align-start',
+  stretch: 'align-stretch',
+};
+
+export const setAlign: IFactoryFunction = (type) => map[type];
