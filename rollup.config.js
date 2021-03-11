84c525392a25ffabb0391859be9e43b331e91336
Christian Farris <christianfarriscs@gmail.com>
2021-03-10 21:26:22 -0600

    create rollup

---
+++ rollup.config.js
@@ -0,0 +1,38 @@
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from 'rollup-plugin-typescript2';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const packageJson = require('./package.json');
+
+const globals = {
+  ...packageJson.devDependencies,
+};
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: packageJson.main,
+      format: 'cjs',
+      sourcemap: true,
+    },
+    {
+      file: packageJson.module,
+      format: 'esm',
+      sourcemap: true,
+    },
+  ],
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    typescript({
+      tsconfig: 'tsconfig.json',
+      useTsconfigDeclarationDir: true,
+      include: ['*.ts+(|x)', '**/*.ts+(|x)', '*.d.ts', '**/*.d.ts'],
+    }),
+  ],
+  external: Object.keys(globals),
+};
