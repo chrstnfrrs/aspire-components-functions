7709895be8798b9145d8e9c32c1aaa957ce71369
Christian Farris <christianfarriscs@gmail.com>
2021-04-07 21:14:47 -0500

    test setColor

---
+++ test/src/factories/setColor.spec.ts
@@ -0,0 +1,13 @@
+import { setColor } from '../../../src/factories/setColor';
+
+import { checkFactory, checkMaterialFactory } from './checkFactory';
+import { expectedColorMap } from './maps/color.spec';
+import { expectedMdColorMap } from './maps/md-color.spec';
+
+const colorMapKeys = Object.keys(expectedColorMap);
+const mdColorMapKeys = Object.keys(expectedMdColorMap);
+
+describe('setColor', () => {
+  checkFactory(setColor, 'setColor', 'color', colorMapKeys);
+  checkMaterialFactory(setColor, 'setColor', mdColorMapKeys);
+});
