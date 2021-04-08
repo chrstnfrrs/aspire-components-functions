1282c044ec0c080497cd7b35798d17817fe1e91a
Christian Farris <christianfarriscs@gmail.com>
2021-04-07 21:14:37 -0500

    create checkMaterialFactory

--- test/src/factories/checkFactory.ts
+++ test/src/factories/checkFactory.ts
@@ -5,7 +5,7 @@ import { IKeyList } from '../types';
 
 const chance = new Chance();
 
-export const checkFactory = (factory: IFactoryFunction, factoryName: string, prop: string, keys: IKeyList): void => {
+const checkFactory = (factory: IFactoryFunction, factoryName: string, prop: string, keys: IKeyList): void => {
   describe.each([...keys])(`When ${factoryName} is called with %s`, (key) => {
     test(`should return ${prop}-${key}`, () => {
       expect(factory(key)).toStrictEqual(`${prop}-${key}`);
@@ -22,3 +22,23 @@ export const checkFactory = (factory: IFactoryFunction, factoryName: string, pro
     });
   });
 };
+
+const checkMaterialFactory = (factory: IFactoryFunction, factoryName: string, keys: IKeyList): void => {
+  describe.each([...keys])(`When ${factoryName} is called with %s`, (key) => {
+    test(`should return md-${key}`, () => {
+      expect(factory(key)).toStrictEqual(`md-${key}`);
+    });
+  });
+  describe(`When ${factoryName} is called with an invalid value`, () => {
+    test('should return undefined', () => {
+      expect(factory(chance.string())).toStrictEqual(undefined);
+    });
+  });
+  describe(`When ${factoryName} is called with undefined`, () => {
+    test('should return undefined', () => {
+      expect(factory(undefined)).toStrictEqual(undefined);
+    });
+  });
+};
+
+export { checkFactory, checkMaterialFactory };
