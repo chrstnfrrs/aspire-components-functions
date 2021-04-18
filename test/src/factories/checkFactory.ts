2d214e7f4b79877e6e613cdd14f18962aa50f388
Christian Farris <christianfarriscs@gmail.com>
2021-04-18 02:53:50 -0500

    add functions to test factories that use array and/or string lists

--- test/src/factories/checkFactory.ts
+++ test/src/factories/checkFactory.ts
@@ -1,11 +1,16 @@
 import Chance from 'chance';
 
-import { IFactoryFunction } from '../../../src/types';
+import { IFactoryArrayFunction, IFactoryFunction } from '../../../src/types';
 import { IKeyList } from '../types';
 
 const chance = new Chance();
 
-const checkFactory = (factory: IFactoryFunction, factoryName: string, prop: string, keys: IKeyList): void => {
+const checkFactory = (
+  factory: IFactoryFunction | IFactoryArrayFunction,
+  factoryName: string,
+  prop: string,
+  keys: IKeyList,
+): void => {
   describe.each([...keys])(`When ${factoryName} is called with %s`, (key) => {
     const validatedKey = key.replaceAll('.', '_').replaceAll('/', '__');
 
@@ -21,6 +26,87 @@ const checkFactory = (factory: IFactoryFunction, factoryName: string, prop: stri
   describe(`When ${factoryName} is called with undefined`, () => {
     test('should return undefined', () => {
       expect(factory(undefined)).toStrictEqual(undefined);
+    });
+  });
+};
+
+const checkFactoryArray = (factory: IFactoryArrayFunction, factoryName: string, prop: string, keys: IKeyList): void => {
+  const pickKeyFromMap = (): string => keys[chance.natural({ max: keys.length - 1 })];
+  let arrayOfKeys: string[];
+
+  describe(`When ${factoryName} is called with an array of valid values`, () => {
+    let result: string | undefined;
+
+    beforeEach(() => {
+      arrayOfKeys = chance.n(pickKeyFromMap, chance.d6());
+      result = factory(arrayOfKeys);
+    });
+    test(`should return all correct classes in a space separated string`, () => {
+      arrayOfKeys.forEach((key) => {
+        expect(result).toContain(`${prop}-${key}`);
+      });
+    });
+  });
+  describe(`When ${factoryName} is called with an array with an invalid value`, () => {
+    let result: string | undefined, validKeys: string[], invalidKeys: string[];
+
+    beforeEach(() => {
+      validKeys = chance.n(pickKeyFromMap, chance.d6());
+      invalidKeys = chance.n(chance.string, chance.d6());
+
+      arrayOfKeys = chance.shuffle([...validKeys, ...invalidKeys]);
+      result = factory(arrayOfKeys);
+    });
+    test(`should return all correct classes and remove invalid classes in a space separated string`, () => {
+      validKeys.forEach((key) => {
+        expect(result).toContain(`${prop}-${key}`);
+      });
+      invalidKeys.forEach((key) => {
+        expect(result).not.toContain(`${key}`);
+      });
+    });
+  });
+};
+
+const checkFactoryStringList = (
+  factory: IFactoryArrayFunction,
+  factoryName: string,
+  prop: string,
+  keys: IKeyList,
+): void => {
+  const pickKeyFromMap = (): string => keys[chance.natural({ max: keys.length - 1 })];
+  let arrayOfKeys: string[];
+
+  describe(`When ${factoryName} is called with a string list of valid values`, () => {
+    let result: string | undefined;
+
+    beforeEach(() => {
+      arrayOfKeys = chance.n(pickKeyFromMap, chance.d6());
+      result = factory(arrayOfKeys.join(' '));
+    });
+    test(`should return all correct classes in a space separated string`, () => {
+      arrayOfKeys.forEach((key) => {
+        expect(result).toContain(`${prop}-${key}`);
+      });
+    });
+  });
+  describe(`When ${factoryName} is called with a string list with an invalid value`, () => {
+    let result: string | undefined, validKeys: string[], invalidKeys: string[];
+
+    beforeEach(() => {
+      validKeys = chance.n(pickKeyFromMap, chance.d6());
+      invalidKeys = chance.n(chance.string, chance.d6());
+
+      arrayOfKeys = chance.shuffle([...validKeys, ...invalidKeys]);
+      result = factory(arrayOfKeys.join(' '));
+    });
+    test(`should return all correct classes and remove invalid classes in a space separated string`, () => {
+      validKeys.forEach((key) => {
+        expect(result).toContain(`${prop}-${key}`);
+      });
+      invalidKeys.forEach((key) => {
+        expect(result).not.toContain(`${key}`);
+      });
     });
   });
 };
@@ -45,4 +131,4 @@ const checkMaterialFactory = (factory: IFactoryFunction, factoryName: string, pr
   });
 };
 
-export { checkFactory, checkMaterialFactory };
+export { checkFactory, checkFactoryArray, checkFactoryStringList, checkMaterialFactory };
