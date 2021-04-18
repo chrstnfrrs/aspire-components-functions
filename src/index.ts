2b97d37a4876df2100cfa0e360b29aaf41fe2bdd
Christian Farris <christianfarriscs@gmail.com>
2021-04-18 00:53:26 -0500

    export functions

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,27 @@
 import { setAlign } from './factories/setAlign';
+import { setBackgroundColor } from './factories/setBackgroundColor';
 import { setColor } from './factories/setColor';
+import { setDisplay } from './factories/setDisplay';
+import { setDirection } from './factories/setDirection';
 import { setJustify } from './factories/setJustify';
+import { setHeight } from './factories/setHeight';
+import { setMaxHeight } from './factories/setMaxHeight';
+import { setMaxWidth } from './factories/setMaxWidth';
+import { setMinHeight } from './factories/setMinHeight';
+import { setMinWidth } from './factories/setMinWidth';
+import { setWidth } from './factories/setWidth';
 
-export { setAlign, setColor, setJustify };
+export {
+  setAlign,
+  setBackgroundColor,
+  setColor,
+  setDirection,
+  setDisplay,
+  setHeight,
+  setJustify,
+  setMaxHeight,
+  setMaxWidth,
+  setMinHeight,
+  setMinWidth,
+  setWidth,
+};
