4a97fbcd0d857e555856f18147c813d2f18da854
Christian Farris <christianfarriscs@gmail.com>
2021-05-15 23:43:49 -0500

    export setElevation

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { setAlign } from './factories/setAlign';
 import { setBackgroundColor } from './factories/setBackgroundColor';
 import { setColor } from './factories/setColor';
-import { setDisplay } from './factories/setDisplay';
 import { setDirection } from './factories/setDirection';
+import { setDisplay } from './factories/setDisplay';
+import { setElevation } from './factories/setElevation';
 import { setHeight } from './factories/setHeight';
 import { setJustify } from './factories/setJustify';
 import { setLayout } from './factories/setLayout';
@@ -20,6 +21,7 @@ export {
   setColor,
   setDirection,
   setDisplay,
+  setElevation,
   setHeight,
   setJustify,
   setLayout,
